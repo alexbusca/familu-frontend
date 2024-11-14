4eaead8ffa2438f697cf6c2225fa2eecc4b2206b
alexbusca <alessandrobuscarinu@gmail.com>
2024-11-14 16:16:27 +0100

    modified main-dashboard

--- src/app/components/main-dashboard.js
+++ src/app/components/main-dashboard.js
@@ -4,7 +4,8 @@ const MainDashboard = () => {
     return (
         <div className="main-dashboard">
             <header className="dashboard-header">
-                <h1>Main Dashboard</h1>
+                <h1>Dashboard</h1>
+                <div></div>
             </header>
             <main className="dashboard-content">
                 {/* Add your main dashboard content here */}
